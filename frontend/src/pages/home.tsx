81f3679a6767085abe1b4ec78ed09f48c8b50eb8
Paul Caron <home@Pauls-MBP.hitronhub.home>
2024-07-07 11:09:37 -0500

    first commit in paul's branch

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,12 @@
+import Metas from '../components/metas.tsx' ;
+
+const Home = () => {
+  return (
+    <>
+      <Metas title="Webler Codes - Home" />
+      <h1>HOME</h1>
+    </>
+  )
+}
+
+export default Home ;
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,12 @@
+import Metas from '../components/metas.tsx' ;
+
+const Login = () => {
+  return (
+    <>
+      <Metas title="Webler Codes - Login" />
+      <h1>Login</h1>
+    </>
+  )
+}
+
+export default Login ;
