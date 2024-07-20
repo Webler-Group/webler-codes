253b534cd60c7f7dfe809c354c21f17094c4598a
Bytenol <ccosmos416@gmail.com>
2024-07-20 12:06:18 -0400

    initial test commit

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,5 +1,19 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    clearMocks: true
+    clearMocks: true,
+    roots: ['./src/test'],
+    testMatch: ['**/*.test.ts'],
+    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    setupFiles: ['./src/test/setup.ts'],
+    coverageDirectory: './coverage',
+    collectCoverageFrom: [
+        'src/ts/**/*.{ts,js}',
+        '!src/ts/**/*.d.ts',
+        '!src/ts/**/server.ts', // exclude server files
+        '!src/ts/**/migrations/**/*', // exclude migration files
+        '!src/ts/**/seed.ts', // exclude seed files
+        '!src/ts/**/codeTemplateSeed.ts', // exclude code-seed files
+    ],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
--- backend/src/test/utils/utils.test.ts
+++
@@ -1,5 +0,0 @@
-describe("This is just a test template", () => {
-    test("Always return true", () => {
-        expect(true).toBe(true);
-    })
-})
