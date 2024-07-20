f287964b6afb5506c6a9a1c791f0232a93dbb7ee
Bytenol <ccosmos416@gmail.com>
2024-07-20 13:07:01 -0400

    stable test for ts/global

--- backend/src/test/setup.ts
+++ backend/src/test/setup.ts
@@ -1,3 +1,10 @@
+/**
+ * Explicit environment settings made here is strictly for the testing environment
+ */
 import {config} from 'dotenv';
 
 config({path: '.env.development'});
+
+// Ensure secrets are set for the test environment
+process.env.ACCESS_TOKEN_SECRET = 'testAccessTokenSecret';
+process.env.REFRESH_TOKEN_SECRET = 'testRefreshTokenSecret';
